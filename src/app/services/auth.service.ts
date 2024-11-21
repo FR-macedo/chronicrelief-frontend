e17a634b5a9c571795c669528af556af1ffb0f6b
Francisco Macedo <macedo270701@gmail.com>
2024-11-21 18:54:00 -0300

    refactor: wrong url was being used

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/users';
+  private apiUrl = 'http://localhost:3100/api/users';
 
   constructor(private http: HttpClient) {}
 
