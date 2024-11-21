2711ee87f64a984fa19a88b27ed280401dc444cb
Francisco Macedo <macedo270701@gmail.com>
2024-11-21 00:34:07 -0300

    feat<auth>: add authGuard, authService

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  template: `
+    <ion-content>
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>Registro</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          <form (ngSubmit)="onSubmit()">
+            <ion-input 
+              [(ngModel)]="nome" 
+              name="nome" 
+              label="Nome" 
+              placeholder="Seu nome"
+            ></ion-input>
+            <ion-input 
+              [(ngModel)]="email" 
+              name="email" 
+              type="email" 
+              label="Email" 
+              placeholder="seu@email.com"
+            ></ion-input>
+            <ion-input 
+              [(ngModel)]="senha" 
+              name="senha" 
+              type="password" 
+              label="Senha"
+            ></ion-input>
+            <ion-button expand="block" type="submit">Registrar</ion-button>
+          </form>
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
+  `
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  nome: string = '';
+  email: string = '';
+  senha: string = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
+  onSubmit() {
+    this.authService.register({
+      nome: this.nome, 
+      email: this.email, 
+      senha: this.senha
+    }).subscribe({
+      next: (response) => {
+        // Tratar sucesso do registro
+      },
+      error: (error) => {
+        // Tratar erro de registro
+      }
+    });
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3000/api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  register(userData: { nome: string; email: string; senha: string }): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, userData);
+  }
+
+  login(credentials: { email: string; senha: string }): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, credentials);
+  }
+
+  isAuthenticated(): boolean {
+    // Exemplo básico para verificar a existência de um token
+    return !!localStorage.getItem('authToken');
+  }
 }
